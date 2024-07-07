af659eb62350bd9ea5d7f2808a17488f7fcee5b1
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2024-07-07 16:07:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def save_patient(patient_info):
         row_data = []
         for header in headers:
             row_data.append(patient_info.get(header, ""))
-        row_num = len(sheet.get_all_records(headers=headers)) + 1
+        row_num = len(sheet.get_all_values()) + 1
         for col, value in enumerate(row_data):
             sheet.update_cell(row_num, col + 1, value)
         st.success("Patient information saved successfully")
